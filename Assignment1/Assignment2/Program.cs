7430d82e193c767066afe6b624f8a82d9bddd40a
abashidzeluka <abashidze2003@gmail.com>
2026-05-16 17:48:52 +0400

    Initial commit

---
+++ Assignment1/Assignment2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                for (int s = 1; s <= num - i; s++)
+                {
+                    Console.Write(" ");
+                }
+
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        
+        }
+    }
+}
