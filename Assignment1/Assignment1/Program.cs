84b3250b3d84b44e875b6fedd45f26ecfe0e4cef
abashidzeluka <abashidze2003@gmail.com>
2026-05-16 17:18:28 +0400

    Invalid Format

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.Design;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your number: ");
+            bool numValid = int.TryParse(Console.ReadLine(), out int num);
+
+            while (!numValid)
+            {
+                Console.Write("invalid format try again:");
+                numValid = int.TryParse(Console.ReadLine(), out num);
+            }
+
+            if (numValid)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"{num}*{i}={num * i}");
+                }
+            }
+                                                                      
+        }
+    }
+}
