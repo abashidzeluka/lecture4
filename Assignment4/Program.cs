5823c72306afbcbfc373283356c50bf0889ca174
abashidzeluka <abashidze2003@gmail.com>
2026-05-16 19:26:30 +0400

    Initial commit

---
+++ Assignment4/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int secret = rnd.Next(1, 101);
+
+            int guess = 0;
+
+            while (guess != secret)
+            {
+                Console.Write("Guess the number (1-100): ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess < secret)
+                {
+                    Console.WriteLine("Higher!");
+                }
+                else if (guess > secret)
+                {
+                    Console.WriteLine("Lower!");
+                }
+                else
+                {
+                    Console.WriteLine("Correct!");
+                }
+            }
+        }
+    }
+}
